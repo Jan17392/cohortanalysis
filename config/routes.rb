ba3ad7c291eb1eebbe1aa7627c3feeaa51bc0788
Jan Runo <jan.runo@whu.edu>
2017-05-02 13:07:09 +0200

    deleting the unneeded template files

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/create'
-
-  get 'users/update'
-
-  get 'users/delete'
-
-  get 'users/new'
-
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
