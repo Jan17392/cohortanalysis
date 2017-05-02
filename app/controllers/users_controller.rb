ea2e05e278a4912540487ab530a389bf5d885e55
Jan Runo <jan.runo@whu.edu>
2017-05-02 12:57:03 +0200

    initial front-end commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def new
+    @user = User.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/delete'
+
+  get 'users/new'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
